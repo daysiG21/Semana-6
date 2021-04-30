f3a3aa9f5f6f41f9f03277cd3930d1b58e441f3f
Daysi <daysi219@gmail.com>
2021-04-29 20:48:29 -0500

    heroku

--- build/config/sequelize.js
+++ build/config/sequelize.js
@@ -7,21 +7,47 @@ exports.conexion = void 0;
 
 var _sequelize = require("sequelize");
 
-const conexion = new _sequelize.Sequelize("almacen", //nombre de base de datos
-"root", //nombre de usuario
-"Pwsqladmin1", //contraseña
+const conexion = process.env.JAWSDB_URL ? new _sequelize.Sequelize(process.env.JAWSDB_URL, {
+  dialect: "mysql",
+  timezone: "-05:00",
+  logging: false,
+  dialectOptions: {
+    dateStrings: true
+  }
+}) : new _sequelize.Sequelize("almacen", // database name
+"root", // username
+"Pwsqladmin1", // password
 {
   dialect: "mysql",
-  //tambien podemos usar para pgadmin, sqlserver, sqlite3, mariadb, mysql
-  // /* one of 'mysql' | 'mariadb' |'postgres' | 'mssql' */ para sqlite se usa connection URI
+  // tambien podemos usar dialectos para pgadmin, sqlserver, sqlite3, mariadb, mysql
+  // /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */ para sqlite se usa connection URI
   host: "127.0.0.1",
   port: 3306,
   timezone: "-05:00",
-  //no funciona en SQLite
+  // no funciona en SQLITE
   dialectOptions: {
-    //sirve para que al momento de mostrar las fechas, automàticamente las convierta en string y no tener que hacer una conversion manual
+    // sirve para que al momento de mostrar las fechas, automaticamente las convierta en string y no tener que hacer una conversion manual
     dateStrings: true
   },
   logging: false
 });
+/*export const conexion = new Sequelize(
+  "almacen",//nombre de base de datos
+  "root",//nombre de usuario
+  "Pwsqladmin1",//contraseña
+  {
+    dialect:"mysql",//tambien podemos usar para pgadmin, sqlserver, sqlite3, mariadb, mysql
+    
+    host:"127.0.0.1",
+    port:3306,
+    timezone:"-05:00",//no funciona en SQLite
+    dialectOptions:{
+      //sirve para que al momento de mostrar las fechas, automàticamente las convierta en string y no tener que hacer una conversion manual
+      dateStrings:true
+    },
+    
+    logging: false,
+  }
+)*/
+
 exports.conexion = conexion;
